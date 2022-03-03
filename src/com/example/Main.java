8db89f4f235f89225b05f5de4be7937024743984
xiaan <xiaan_b@hotmail.com>
2022-03-04 09:48:49 +1300

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Float min " + myMinFloatValue);
+        System.out.println("Float max " + myMaxFloatValue);
+
+
+        Double myMinDoubleValue = Double.MIN_VALUE;
+        Double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double min " + myMinDoubleValue);
+        System.out.println("Double max " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+    }
+}
