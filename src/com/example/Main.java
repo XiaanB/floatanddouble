1297c1bd7ab087ba5aeb4ca94eea33374457f9ee
xiaan <xiaan_b@hotmail.com>
2022-03-04 10:13:32 +1300

    Pounds to kilograms

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -16,8 +16,16 @@ public class Main {
         System.out.println("Double min " + myMinDoubleValue);
         System.out.println("Double max " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5/3;
+        float myFloatValue = 5f/3f;
+        double myDoubleValue = 5d/3d;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        double numberOfPounds = 1;
+        double convertedKilograms = (numberOfPounds * .45359237);
+        System.out.println("pounds to kilograms = " + convertedKilograms);
     }
 }
